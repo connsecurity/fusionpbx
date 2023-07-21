e01f886402ae511ff9f2c3d86bdabb9b00c621ba
Yuji Sasazaki <jhonatan-yuji@hotmail.com>
2023-07-21 15:44:31 -0300

    Also load user info to session

--- app/chatwoot_api/resources/chatwoot_api.php
+++ app/chatwoot_api/resources/chatwoot_api.php
@@ -2,14 +2,14 @@
 
 require "chatwoot_api_functions.php";
 
-if (isset($_SESSION['chatwoot']['account']['id'])) {
-    if ($_SESSION['chatwoot']['account']['domain_uuid'] !==  $_SESSION['domain_uuid']) {
+if (!isset($_SESSION['chatwoot']['account']['domain_uuid'])
+    || $_SESSION['chatwoot']['account']['domain_uuid'] !==  $_SESSION['domain_uuid']) {
+
+        $user = chatwoot_user::get_user_by_uuid($_SESSION['user_uuid']);
 
         $_SESSION['chatwoot']['account']['id'] = chatwoot_account::get_domain_account_id();
         $_SESSION['chatwoot']['account']['domain_uuid'] =  $_SESSION['domain_uuid'];
-    }
-} else {
-    
-    $_SESSION['chatwoot']['account']['id'] = chatwoot_account::get_domain_account_id();
-    $_SESSION['chatwoot']['account']['domain_uuid'] =  $_SESSION['domain_uuid'];
+        $_SESSION['chatwoot']['user']['id'] = $user['user_id'];
+        $_SESSION['chatwoot']['user']['access_token'] = $user['access_token'];
+        $_SESSION['chatwoot']['user']['pubsub_token'] = $user['pubsub_token'];
 }

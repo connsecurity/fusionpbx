d7b6b26663f2764b318a33345204f66ce9e9507d
Yuji Sasazaki <jhonatan-yuji@hotmail.com>
2023-07-21 15:24:23 -0300

    Load variables from the API and move the html code to index.php

--- app/customer_chat/index.php
+++ app/customer_chat/index.php
@@ -1,30 +1,61 @@
 <?php
 
 //includes
-	require_once dirname(__DIR__, 2) . "/resources/require.php";
-	require_once "resources/check_auth.php";
+require_once dirname(__DIR__, 2) . "/resources/require.php";
+require_once "resources/check_auth.php";
+require "app/chatwoot_api/resources/chatwoot_api.php";
 
 //check permissions
-	if (permission_exists('customer_chat_view')) {
-		//access granted
-	}
-	else {
-		//echo "access denied";
-		//exit;
-	}
+if (permission_exists('customer_chat_view')) {
+	//access granted
+} else {
+	//echo "access denied";
+	exit;
+}
 
 //add multi-lingual support
-	$language = new text;
-	$text = $language->get();
+$language = new text;
+$text = $language->get();
 
-	readfile("body.html");
+//show content
+require_once "resources/header.php";
 
 ?>
 
-<script src="./test_variables.js?v=<?=time();?>"></script>
-<script src="./app.js?v=<?=time();?>"></script>
+<div id="customer_chat">
+	<div id="conversation_list">
+		<div id="username"><?= $_SESSION['username'] ?></div>
+	</div>
+	<div id="chat">
+		<div id="chat_header">
+			<span id="contact_name">Contact Name</span>
+			<div id="actions">
+				<div id="open_actions">
+					<button id="resolve_button">Resolve</button>
+					<button id="options_button">?</button>
+				</div>
+			</div>
+		</div>
+		<div id="chat_messages"></div>
+		<form id="message_form">
+			<textarea id="message_input" placeholder="Type your message..."> </textarea>
+			<button type="submit" id="send_button" class="fas fa-paper-plane"></button>
+		</form>
+	</div>
+</div>
+
+<script>
+window.chatwoot = {};
+chatwoot.chatwoot_api_url = "<?= $_SESSION['chat']['chatwoot_url']['text'] . "/api/v1" ?>";
+chatwoot.websocket_url = "<?= str_replace("https", "wss", $_SESSION['chat']['chatwoot_url']['text']) . "/cable" ?>";
+chatwoot.account_id = "<?= $_SESSION['chatwoot']['account']['id'] ?>";
+chatwoot.user_id = "<?= $_SESSION['chatwoot']['user']['id']  ?>";
+chatwoot.user_api_access_token = "<?= $_SESSION['chatwoot']['user']['access_token'] ?>";
+chatwoot.contact_pubsub_token = "<?= $_SESSION['chatwoot']['user']['pubsub_token'] ?>";
+</script>
+<script src="./app.js?v=<?= time(); ?>"></script>
 
 <?php
 //include the footer
-	require_once "resources/footer.php";
+require_once "resources/footer.php";
 ?>
